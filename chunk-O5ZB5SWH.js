6c839a1b4bfe3920c73fcadc4715a2bb14fb5017
lifelinest <137246924+lifelinest@users.noreply.github.com>
2025-08-28 15:07:50 +0000

    Deploying to gh-pages from @ lifelinest/nav@f6441daefbf0fe6e68cf909df44794bef33b94bb 🚀

---
+++ chunk-O5ZB5SWH.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-IT24O2YX.js";import{pb as r,yb as e}from"./chunk-LAJ5ITAT.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
